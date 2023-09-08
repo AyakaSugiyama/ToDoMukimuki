ca94deeb983c4bdb75fc311f7336d554a34d5f19
kokoro-0820 <221205056@ccmailg.meijo-u.ac.jp>
2023-09-08 11:36:48 +0900

    ボタン機能実装

--- app/src/main/java/jp/ac/meijou/android/todomukimuki/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/todomukimuki/MainActivity.java
@@ -2,13 +2,28 @@ package jp.ac.meijou.android.todomukimuki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import jp.ac.meijou.android.todomukimuki.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int p = 0;
+
+        binding.switch1.setOnClickListener(v -> {
+            binding.point.setText("1");
+        });
+
+
     }
 }
