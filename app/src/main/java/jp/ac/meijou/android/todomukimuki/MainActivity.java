ddcde8d04a1b8fe6126f32932c7ab1cc6d938308
First-name Family-name <221205082@ccmailg.meijo-u.ac.jp>
2023-09-08 13:53:13 +0900

    コミットだけする intｐの位置を変えた

--- app/src/main/java/jp/ac/meijou/android/todomukimuki/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/todomukimuki/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private int imageRes = R.drawable.mukimuki_level1;
 
+    int p = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
